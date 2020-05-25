9ee3edcfefd53050f41c8ac84cdf55c9b15c13ae
hansolnoh95 <hansolnoh95@naver.com>
2020-05-25 21:45:44 +0900

    delegate, datasource applied

--- iOS_FifthWeek_Seminar_Assignment/iOS_FifthWeek_Seminar_Assignment/MainViewController.swift
+++ iOS_FifthWeek_Seminar_Assignment/iOS_FifthWeek_Seminar_Assignment/MainViewController.swift
@@ -14,6 +14,8 @@ class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setCardList()
+        self.cardCollectionView.delegate = self
+        self.cardCollectionView.dataSource = self
 
         // Do any additional setup after loading the view.
     }
@@ -22,6 +24,8 @@ class MainViewController: UIViewController {
     
     private func setCardList() {
         let card1 = Card(dday: "전역일 D- 435", ddate: "2021-12-31", imgName: "mainImgPic", typeName: "mainiconArmy", name: "박준석 훈련병", pic1Name: "mainJpgMask1", pic2Name: "mainJpgMask2", pic3Name: "mainJpgMask3")
+        let card2 = Card(dday: "전역일 D- 435", ddate: "2021-12-31", imgName: "mainImgPic", typeName: "mainiconArmy", name: "박준석 훈련병", pic1Name: "mainJpgMask1", pic2Name: "mainJpgMask2", pic3Name: "mainJpgMask3")
+        let card3 = Card(dday: "전역일 D- 435", ddate: "2021-12-31", imgName: "mainImgPic", typeName: "mainiconArmy", name: "박준석 훈련병", pic1Name: "mainJpgMask1", pic2Name: "mainJpgMask2", pic3Name: "mainJpgMask3")
     }
 
     /*
@@ -34,4 +38,30 @@ class MainViewController: UIViewController {
     }
     */
 
+}
+
+extension MainViewController: UICollectionViewDataSource {
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return cardList.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cardCell = collectionView.dequeueReusableCell(withReuseIdentifier: CardCell.identifier, for: indexPath) as? CardCell else {return UICollectionViewCell()}
+        cardCell.set(cardList[indexPath.row])
+        return cardCell
+    }
+}
+
+extension MainViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width:306, height:536)
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 19, left: 35, bottom: 46, right: 34)
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 12
+    }
+    
 }
